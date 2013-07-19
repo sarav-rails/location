065bb36b7c91fe9caf44f13e8fe3c12a2995b4dd
Saravanan K <saravani20@gmail.com>
2013-07-19 13:59:46 +0530

    location based content- initial setup

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,14 @@
+class PaymentsController < ApplicationController
+	def index
+	end
+	
+	def payment
+		#test api key
+		#1e704e14f04411e28144026ba7d31e6f
+		#client test api key
+		#
+		#client original key
+		
+	end
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Location::Application.config.secret_token = '576e258a908789d0bdfa79c50d454bc9b2b710d72bcf77123be316f84270616e9c9ba05550f04e10218ab6997bfffdd6a38941c49067c2bd8fdbbd03afd0fe3e'
